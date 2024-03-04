56edec7d60f6fa81bd800da9484548540b44cf6b
ridhdhi2510 <162030525+ridhdhi2510@users.noreply.github.com>
2024-03-04 08:31:59 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+//alert(3+3)-->error occure bcz We are using nodejs, not browser
+
+let name = "ridhdhi" // dataType --> string
+let age = 18   // dataType --> number
+let isLoggedIn = true //dataType --> boolean
+//null --> standalone value 
+
+let state; //datatype --> undifined
+
+
+//object 
+
+console.log(typeof "ridhhdi"); 
+console.log(typeof age);
+console.log(typeof null);   //object
+console.log(typeof undefined);  //undefined
