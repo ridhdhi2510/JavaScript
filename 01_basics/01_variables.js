5f8812b765d77bf4fcc50a03f7c7da77e3625d77
ridhdhi2510 <162030525+ridhdhi2510@users.noreply.github.com>
2024-03-04 08:32:30 +0000

    vriable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1234567
+let accountEmail = "ridhdhi@gmila.com"
+var accountPassword = "234"
+accountCity = "Bhavnagar"
+let accountState;
+
+//accountId=345 --> not Allowed
+console.log(accountId);
+
+accountEmail = "haresh@gamil.com"
+accountPassword ="567"
+accountCity="ahemdabad"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+console.log(accountState)
