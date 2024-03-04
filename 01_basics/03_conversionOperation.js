8a19c7f71ef00fcb5b52edb50cf65e1850d0668f
ridhdhi2510 <162030525+ridhdhi2510@users.noreply.github.com>
2024-03-04 11:41:46 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,14 +1,46 @@
 let score = "33"
 
-console.log(typeof(score))
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
 
-console.log(typeof(valueInNumber))
+// console.log(typeof(valueInNumber))
 
 
 let name1 = "123Ridhdhi"
-console.log(typeof(name1))   // string
+// console.log(typeof(name1))   // string
 let name2 = Number(name1)    // string -->number
-console.log(typeof(name2))   // number
-console.log(name2)           // NaN 
+// console.log(typeof(name2))   // number
+// console.log(name2)           // NaN 
+
+
+//*************************OPERATION****************************
+
+let value = 3
+let negValue = -3
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*3)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " ridhdhi"
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" + 2); //12
+// console.log(4 + "5"); //45
+// console.log("1" + 2 + 2) //122
+// console.log(1 + 2 + "2") /32
+
+
+// console.log(+true)   // 1
+// console.log(+"")     // 0
+
+let counter = 100
+counter++
+console.log(counter)   //101
