0dd86db0a53140e0c8659b2fba51ba1aaf0f0558
ridhdhi2510 <162030525+ridhdhi2510@users.noreply.github.com>
2024-03-04 11:08:36 +0000

    Conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "33"
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber))
+
+
+let name1 = "123Ridhdhi"
+console.log(typeof(name1))   // string
+let name2 = Number(name1)    // string -->number
+console.log(typeof(name2))   // number
+console.log(name2)           // NaN 
