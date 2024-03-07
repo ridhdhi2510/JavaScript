5f20a7487a3824d053074ea27739a348eb1128f4
ridhdhi2510 <162030525+ridhdhi2510@users.noreply.github.com>
2024-03-07 05:40:41 +0000

    object part 1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,36 @@
+//object literals
+const jsuser = {
+    name : "ridhdhi",
+    age : 20,
+    email : "ridhhdi@goggle.com",
+    isLoggedIn : false,
+    lastloginDay: ["monday","friday"]
+ }
+
+// console.log(jsuser.email);    //ridhhdi@goggle.com
+// console.log(jsuser["email"]); //ridhhdi@goggle.com
+
+jsuser.email = "ridhdhi@yahoo.com"
+Object.freeze(jsuser)
+        //Prevents the modification of existing property attributes and values,
+        // and prevents the addition of new properties.
+jsuser.email ="ridhdhi@ac.in"
+// console.log(jsuser)
+
+
+const detail ={
+    name : "ridhdhi",
+    age : 20 ,
+    year : 3
+}
+detail.greeting = function(){
+    console.log("hello !!!");
+}
+
+// console.log(detail.greeting)   //[Function (anonymous)]
+// console.log(detail.greeting())   //hello !!!
+
+detail.greeting2 = function(){
+    console.log(`hello !!!  ${this.name}`);
+}
+console.log(detail.greeting2())  //hello !!!  ridhdhi
