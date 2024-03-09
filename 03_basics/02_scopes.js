61eb9f5f752ff76f8c2585cf4f5a6cce8e90474d
ridhdhi2510 <162030525+ridhdhi2510@users.noreply.github.com>
2024-03-09 09:48:44 +0000

    about scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,20 @@
+function one(){
+    const userName = "ridhdhi"
+    
+    function two(){
+        const website = "youtube"
+        // console.log(userName)
+    }
+    two()
+}
+one()
+
+
+
+if(true){
+    const userName = "ridhdhi"
+    if(userName === "ridhdhi"){
+        const website = "youtube"
+        console.log(userName + website);
+    }
+}
